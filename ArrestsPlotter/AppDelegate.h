845372dd46a6540cfb411cf33659ab11d8fecb18
Mike Berlin <mikeberlin@me.com>
2013-04-24 18:01:12 -0500

    Initial Commit

---
+++ ArrestsPlotter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArrestsPlotter
+//
+//  Created by Mike Berlin on 4/24/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArrestsPlotter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ArrestsPlotter
+//
+//  Created by Mike Berlin on 4/24/13.
+//  Copyright (c) 2013 Mike Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
